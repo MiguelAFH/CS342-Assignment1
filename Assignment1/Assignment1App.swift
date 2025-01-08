eb03ca03f354d76aac4222577fb1c4e25d5c38f0
MiguelAFH <miguel.angel.fh@hotmail.com>
2025-01-07 20:46:23 -0800

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Miguel Fuentes on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1Tests/Assignment1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1Tests.swift
+//  Assignment1Tests
+//
+//  Created by Miguel Fuentes on 1/7/25.
+//
+
+import Testing
+@testable import Assignment1
+
+struct Assignment1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
